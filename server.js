66792ed2c5c357eb58061ab808da97997f46c2b2
Dan Rosenbaum <drosenbaum5@gmail.com>
2021-12-30 07:41:07 -0500

    Created routes folder, index.js file for bundling all routes (api or other), linked to api folder, created test.js and one get route

--- server.js
+++ server.js
@@ -1,7 +1,19 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const routes = require("./routes");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(routes);
+
+mongoose.connect("mongodb://localhost/restAPI", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 app.listen(PORT, () => {
   console.log(`Listening at http://localhost:${PORT}`);
