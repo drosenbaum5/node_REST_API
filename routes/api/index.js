0d40b95131d57c873bf823474a32ae0f1ba211aa
Dan Rosenbaum <drosenbaum5@gmail.com>
2021-12-30 07:49:00 -0500

    Added some basic comments to the routes folder

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,6 +1,9 @@
 const router = require("express").Router();
+// import all api routes here
 const testRoutes = require("./test");
 
+// add more API routes here once more files have been created
+// all routes are exported from this file
 router.use("/test", testRoutes);
 
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 const router = require("express").Router();
+// Import all routes, api or otherwise
 const apiRoutes = require("./api");
 
+// use all imported routes here so they are available to the router
 router.use("/api", apiRoutes);
 
 module.exports = router;
